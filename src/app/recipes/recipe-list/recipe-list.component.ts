a6546d876befe7f6549a2d19a10da79c9a58e1bb
justmaksit <maksym.fedorenko@jetsoftpro.com>
2022-02-17 15:01:32 +0200

    add recipes, html code

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+recipes: Recipe[] = [
+  new Recipe('A Test Recipe', 'This is a test', 'https://www.freeiconspng.com/thumbs/recipes-icon-png/recipe-book-png-file-3.png'),
+  new Recipe('A Test Recipe', 'This is a test', 'https://www.freeiconspng.com/thumbs/recipes-icon-png/recipe-book-png-file-3.png'),
+];
 
   constructor() { }
 
